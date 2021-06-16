aa1fec8ebeb152324f0e630bafc81c26ff46e626
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-06-15 23:21:20 -0500

    kept getting undefined error in readme. rewrote entire code inseparate vscode, got it working, then copy pasted it into this project which now works

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# README-Generator
+## asdf   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+
+## Description
+asdf.
+
+## Table of contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributin](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+asdf.
+
+## Usage
+asdf.
+
+## License
+MIT,Apache,GNU.
+
+## Contributing
+asdf
+
+## Tests
+
+## Questions
+[A link to my gitHub](https://github.com/asdf)  
+Email: asdf
--- index.js
+++ index.js
@@ -1,89 +1,92 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateREADME = (answers) =>
+    `## ${answers.title}   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+
+## Description
+${answers.description}.
+
+## Table of contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributin](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.install}.
+
+## Usage
+${answers.usage}.
+
+## License
+${answers.license}.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+
+## Questions
+[A link to my gitHub](https://github.com/${answers.gitHub})  
+Email: ${answers.email}`
 
 inquirer.prompt([{
             type: 'input',
             name: 'title',
-            message: '',
+            message: 'What is the title of the project?',
         },
         {
             type: 'input',
             name: 'description',
-            message: '',
+            message: 'Description of the project.',
         },
         {
             type: 'input',
             name: 'install',
-            message: '',
+            message: 'What are the installation instructions?',
         },
         {
             type: 'input',
             name: 'usage',
-            message: '',
+            message: 'What are the usage instructions?',
         },
         {
             type: 'checkbox',
             name: 'license',
-            message: '',
+            message: 'what license would you like?',
+            choices: ['MIT', 'Apache', 'GNU'],
         },
         {
             type: 'input',
             name: 'contributing',
-            message: '',
+            message: 'What are the instructions for contributing?',
         },
         {
             type: 'input',
             name: 'test',
-            message: '',
+            message: 'Do you have any tests to add?'
         },
         {
             type: 'input',
-            name: 'github',
-            message: '',
+            name: 'gitHub',
+            message: 'What is your github username'
         },
         {
             type: 'input',
             name: 'email',
-            message: '',
+            message: 'What is an email people can send questions to?'
         }
+
 
     ])
     .then((answers) => {
         const READMEcontent = generateREADME(answers);
         fs.writeFile('README.md', READMEcontent, (err) =>
-            err ? console.log(err) : console.log('Successfully created README.md'));
+            err ? console.log(err) : console.log('Successfully created README.md')
+        );
     })
-
-const gnerateREADME = (answers) =>
-    `## ${answers.title} [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-    
-    ## Description
-    ${answers.desciption}.
-    
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    
-    ## Installation
-    ${answers.install}.
-
-    ## Usage
-    ${answers.usage}.
-
-    ## License
-    ${answers.license}.
-
-    ## Contributing
-    ${answers.contributing}.
-
-    ## Tests
-
-    ## Questions
-    [A link to my gitHub](https://gitbut.com/${answers.github})
-    Email: ${answers.email}
-    `
